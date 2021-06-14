e988cc29c9481fdb3b43e98863cd3cb5981cf908
kerschdan <d.kerschhofer@gmail.com>
2021-06-14 14:06:26 +0200

    Removed superfluous lines of code

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import time
-
 from bs4 import BeautifulSoup
 import re
 from selenium import webdriver
 import pandas as pd
 import random
 
-
 PATH = r'C:\Program Files (x86)\chromedriver.exe'
 
 driver = webdriver.Chrome(executable_path=PATH)
 
-url = "https://www.immowelt.at/liste/wien-3-landstrasse/wohnungen/kaufen?prima=350000&wflmi=50&bjma=1945&sort=createdate%2Bdesc"
+url = "https://www.immowelt.at/liste/wien-3-landstrasse/wohnungen/kaufen?prima=350000&wflmi=50&bjma=1960&sort=createdate%2Bdesc"
 # up to 350000€
-# built 1945 or earlier
+# built 1960 or earlier
 # 50 square meters or more
 
 driver.get(url)
 time.sleep(random.randrange(4, 6)*1.15)
+
 
 # Handling Cookies
 # Cookies are processed via the Consent Management Platform usercentrics
@@ -29,22 +28,17 @@ element.click()
 time.sleep(random.randrange(3, 5)*1.15)
 
 
+# Dealing with AJAX elements
 # The website loads part of the content when scrolling down
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") # scrolls to bottom of page
 time.sleep(random.randrange(3, 6)*1.05)
 
 
+# Parsing the document
 pageSource = driver.page_source
 driver.close()
 
 bsObj = BeautifulSoup(pageSource, "html.parser")
-
-#### Necessary to combine both Tag Objects but this is not possible
-# Remedy: Convert to htmL_doc that can be
-# parsed by BeautifulSoup to create a new bsObj
-tagObj_static = bsObj.find(id="listItemWrapperFixed")
-tagObj_dynamic = bsObj.find(id="listItemWrapperAsync")
-#######################
 
 list_links = []
 tagObj_href = bsObj.find_all("a", href=re.compile("expose"))
@@ -54,7 +48,7 @@ for i in tagObj_href:
 
 print(list_links)
 
-'''
+
 # Setting up lists that form the dataframe
 link_expose = [] # works as ID
 price = []
@@ -66,22 +60,14 @@ squaremeters = []
 rooms = []
 #buildYear = []
 
+
+# Getting links to all listing pages
 for i in list_links:
     link_expose.append("https://www.immowelt.at" + i)
 print(link_expose)
 
-link_expose1 = link_expose[1]
-print(link_expose1)
 
-link_expose2 = link_expose[0]
-print(link_expose2)
-
-link_expose = []
-link_expose.append(link_expose1)
-link_expose.append(link_expose2)
-print("New list with a reduced number of expose:")
-print(link_expose)
-
+# Getting the data from each listing
 for i in link_expose:
     driver = webdriver.Chrome(executable_path=PATH)
     driver.get(i)
@@ -125,10 +111,9 @@ for i in link_expose:
     rooms.append(rooms_item)
 
 
-# Creating the dataframe
+# Creating the dataframe and writing to a csv file
 cols = ['Price', 'Squaremeters', 'Rooms']
 df = pd.DataFrame({'Price':price,
                    'Squaremeters':squaremeters,
                    'Rooms':rooms})[cols]
 df.to_csv('Vienna_raw.csv')
-'''
